081cecf9539b78e784374ef9320e7e223542ba43
mew8030 <mew8030@proton.me>
2025-12-23 00:45:08 -0500

    A2: added reading capability for the agent so that it can read directories and files

---
+++ gemi/functions/get_file_content.py
@@ -0,0 +1,23 @@
+import os
+from config import LIMIT
+def get_file_content(working_directory, file_path):
+    try:
+        abwdir = os.path.abspath(working_directory)
+        target_file = os.path.normpath(os.path.join(abwdir, file_path))
+
+        valid_target_file = os.path.commonpath([abwdir, target_file]) == abwdir
+        if not valid_target_file:
+            return f'    Error: Connot read "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(target_file):
+            return f'    Error: File not found or is not a regular file: "{file_path}"'
+
+        content = ""
+        with open(target_file) as f:
+            print(f"reading file")
+            content = f.read(LIMIT)
+            if f.read(1):
+                content += f'[...File "{file_path}" truncated at {LIMIT} characters]'
+            print(f"reading complete")
+        return content
+    except Exception as e:
+        return f"    Error: {e}"
