267b2ffe87191f94ee7f95d560e33ac0c818c138
mew8030 <mew8030@proton.me>
2025-12-24 13:24:48 -0500

    A5: finished creating an ai agent that has the ability to read write and make decisions within a restrained location of a directory

--- gemi/call_function.py
+++ gemi/call_function.py
@@ -1,8 +1,8 @@
 from google.genai import types
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python_file import schema_run_python_file
-from functions.write_file import schema_write_file
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.get_file_content import schema_get_file_content, get_file_content
+from functions.run_python_file import schema_run_python_file, run_python_file
+from functions.write_file import schema_write_file, write_file
 
 available_functions = types.Tool(
     function_declarations=[
@@ -12,3 +12,42 @@ available_functions = types.Tool(
         schema_write_file
         ],
 )
+
+
+def call_function(function_call, verbose=False):
+    """handles the abstract task of calling one of the four functions"""
+    function_map = {
+        "get_file_content": get_file_content,
+        "get_files_info": get_files_info,
+        "run_python_file": run_python_file,
+        "write_file": write_file
+    }
+    if verbose:
+        print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(f" - Calling function: {function_call.name}")
+    fun_name = function_call.name or ""
+    if not fun_name or not fun_name in function_map:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {fun_name}"},
+                )
+            ],
+        )
+    fun_args = dict(function_call.args) if function_call.args else {}
+    fun_args["working_directory"] = "./calculator"
+    function_result = function_map[fun_name](**fun_args)
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=fun_name,
+                response={'result': function_result},
+            )
+        ],
+    )
+
+
--- gemi/main.py
+++ gemi/main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 
 
@@ -19,10 +19,6 @@ def main():
     args = parser.parse_args()
     # Now we can access 'args.user_prompt'
 
-    #create the role structure
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    
-    
     #get the api key
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
@@ -31,28 +27,52 @@ def main():
         print("environment variable was found")
     client = genai.Client(api_key=api_key) 
     
-    #call to generate content
-    response = client.models.generate_content(
-        model='gemini-2.5-flash', 
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions],
-            system_instruction=system_prompt
-            ),
-    )
+    #create the role structure
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    
+    for i in range(20):
+        try:
+            #call to generate content
+            response = client.models.generate_content(
+                model='gemini-2.5-flash', 
+                contents=messages,
+                config=types.GenerateContentConfig(
+                    tools=[available_functions],
+                    system_instruction=system_prompt
+                    ),
+            )
 
-    if response.candidates[0].content.parts[0].function_call:
-        for part in response.candidates[0].content.parts:
-            if part.function_call:
-                print(f"Calling function: {part.function_call.name}({part.function_call.args})")
-    else:
-        print(response.text)
+            for candidate in response.candidates:
+                messages.append(candidate.content)
 
-    data = response.usage_metadata
-    if not data:
-        raise ValidationError("missing metadat from response")
-    if args.verbose:
-        print(f"User prompt:{args.user_prompt}\nPrompt tokens: {data.prompt_token_count}\nResponse tokens: {data.candidates_token_count}")
+            #check if there are function calls
+            if response.candidates[0].content.parts[0].function_call:
+                fun_results_list = []
+                #execute each function call
+                for part in response.candidates[0].content.parts:
+                    if part.function_call:
+                        function_call_results = call_function(part.function_call, args.verbose)
+                        if not function_call_results.parts:
+                            raise Exception("parts list empty in function_call_results")
+                        if not function_call_results.parts[0].function_response:
+                            raise Exception("FunctionResponse is somehow type None")
+                        fun_results_list.append(function_call_results.parts[0])
+                        if args.verbose:
+                            print(f"-> {function_call_results.parts[0].function_response.response}")
+                messages.append(types.Content(role="user", parts=fun_results_list))
+            else:
+                if response.text:
+                    print(response.text)
+                    break
+        except Exception as e:
+            print(f"Error: {e}")
+            break
+
+        data = response.usage_metadata
+        if not data:
+            raise ValidationError("missing metadat from response")
+        if args.verbose:
+            print(f"User prompt:{args.user_prompt}\nPrompt tokens: {data.prompt_token_count}\nResponse tokens: {data.candidates_token_count}")
 
 
 if __name__ == "__main__":
